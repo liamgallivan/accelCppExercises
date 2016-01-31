943ff2b5efa42f9b7f1c7314df5fa712855b3da5
Liam Gallivan <gallivanster@gmail.com>
2016-01-30 20:06:40 -0800

    2-3 and 2-4

---
+++ chp2/2-4.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+  cout << "Please enter your first name: ";
+  string name;
+  cin >> name;
+  
+  const string greeting = "Hello, " + name + "!";
+  const int pad = 1;
+  const int rows = pad * 2 + 3;
+  const string::size_type cols = greeting.size() + pad * 2 + 2;
+  cout << endl;
+
+  for (int row = 0; row != rows; ++row) {
+    string::size_type column = 0;
+    string output;
+    while (column != cols) {
+      if (row == pad + 1 && column == pad + 1) {
+        output += greeting;
+        column += greeting.size();
+      } else {
+        if (row == 0 || row == rows -1 ||
+            column == 0 || column == cols -1)
+            output += "*";
+        else 
+            output += " ";
+        ++column;
+      }
+    }
+
+    cout << output << endl;
+  }
+
+  return 0;
+}
